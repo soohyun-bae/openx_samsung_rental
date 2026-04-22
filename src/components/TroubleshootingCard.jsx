9316c6b53a3a2f333642efdc6b3c20cf09c9200b
soohyun-bae <qosuh58@gmail.com>
2026-04-22 15:06:05 +0900

    Rtoubleshooting card gap edit

--- src/components/TroubleshootingCard.jsx
+++ src/components/TroubleshootingCard.jsx
@@ -2,39 +2,36 @@ import { troubleshootingCard } from "../datas/troubleshooting.js";
 
 export const TroubleshootingCard = () => {
   return (
-    <div className="w-full flex-row-center">
-      <div className="flex-col-center gap-8 w-full lg:flex-row lg:gap-15">
+    <div className="flex-row-center w-full">
+      <div className="flex-col-center w-full gap-8 lg:flex-row lg:gap-15">
         {troubleshootingCard.map((item, index) => (
-          <div
-            className="w-full flex-col-center gap-5.5"
-            key={index}
-          >
+          <div className="flex-col-center w-full gap-5.5" key={index}>
             <div
               className={`${
                 index === 0
                   ? "bg-light-black font-regular"
                   : "bg-blue font-bold"
-              } text-white px-8.75 py-3.25 rounded-[58px] text-middle text-nowrap leading-none`}
+              } text-middle rounded-[58px] px-8.75 py-3.25 leading-none text-nowrap text-white`}
             >
               {item.title}
             </div>
             <div
               className={`${
                 index === 0
-                  ? " bg-light-gray font-normal"
+                  ? "bg-light-gray font-normal"
                   : "bg-white font-semibold lg:py-10"
-              } border border-border-gray flex flex-col text-md text-light-black p-10 rounded-xl lg:w-full md:items-center`}
+              } border-border-gray text-md text-light-black flex flex-col rounded-xl border p-10 md:items-center lg:w-full`}
             >
               <div
-                className={`${index === 0 ? "space-y-6" : "space-y-6"} w-max `}
+                className={`${index === 0 ? "space-y-3" : "space-y-3"} w-max`}
               >
                 {item.content.map((content, contentIndex) => (
                   <div className="w-max" key={contentIndex}>
                     {index === 0 ? (
                       <div className="flex-row-center md:w-full">
-                        <div className="flex-row-center gap-4.5 w-max">
+                        <div className="flex-row-center w-max gap-4.5">
                           <img
-                            className="w-6.25 h-6.25"
+                            className="h-6.25 w-6.25"
                             src="https://res.cloudinary.com/dnigvhehc/image/upload/v1776665114/x-icon_v6lctg.png"
                           />
                           <p className="font-regular text-md relative z-5">
@@ -43,15 +40,13 @@ export const TroubleshootingCard = () => {
                         </div>
                       </div>
                     ) : (
-                      <div className="flex items-center gap-2.75 lg:gap-7.5 relative">
-                        <div
-                          className="hidden lg:block bg-linear-to-r from-white to-blue absolute h-px z-0 w-[14vw] 2xl:w-[16vw] right-[calc(100%-0.4rem)]"
-                        />
+                      <div className="relative flex items-center gap-2.75 lg:gap-7.5">
+                        <div className="to-blue absolute right-[calc(100%-0.4rem)] z-0 hidden h-px w-[14vw] bg-linear-to-r from-white lg:block 2xl:w-[16vw]" />
                         <img
-                          className="w-6 h-6"
+                          className="h-6 w-6"
                           src="https://res.cloudinary.com/dnigvhehc/image/upload/v1776664636/blue-checkbox_f6p3cc.png"
                         />
-                        <p className="font-semibold text-md relative z-10">
+                        <p className="text-md relative z-10 font-semibold">
                           {content}
                         </p>
                       </div>
