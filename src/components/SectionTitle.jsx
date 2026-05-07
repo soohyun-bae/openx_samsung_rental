d658dc6e6a350a79622386b83942d7327a87b744
soohyun-bae <qosuh58@gmail.com>
2026-05-07 15:38:09 +0900

    section title edit

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,13 +1,13 @@
 export const SectionTitle = ({ en, ko, description, zIndex = false }) => {
   return (
     <div
-      className={`${zIndex ? "relative z-10" : ""} flex-col-center gap-[35px] text-center break-keep md:gap-7.5`}
+      className={`${zIndex ? "relative z-10" : ""} flex-col-center gap-[clamp(25px,calc((40/1920)*100vw),40px)] text-center break-keep md:gap-7.5`}
     >
-      <p className="text-md text-light-blue leading-none font-semibold">{en}</p>
-      <h3 className="font-line from-dark-blue to-blue text-ko-section-title leading-sectionTitle-ko bg-linear-to-b bg-clip-text font-bold text-nowrap text-transparent">
+      <p className="text-light-blue text-xs leading-none font-semibold">{en}</p>
+      <h3 className="font-line to-light-blue from-border-blue text-ko-section-title leading-sectionTitle-ko bg-linear-to-b bg-clip-text font-bold text-nowrap text-transparent">
         {ko}
       </h3>
-      <p className="text-md font-regular leading-[1.4] text-[#747474] md:leading-none">
+      <p className="font-regular text-sm leading-[1.4] text-[#747474] md:leading-none">
         {description}
       </p>
     </div>
